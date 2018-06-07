e2c9e4a61e6e0698b1edfe2b8ffbdb30c6b02c55
Hannestly <dooley994@gmail.com>
2018-06-07 17:00:06 -0400

    showing radon at interval angles

--- radon.py
+++ radon.py
@@ -5,23 +5,27 @@ import math
 
 
 
-def getRadon(img,angle,size):
+def getRadonAtAngle(img,angle,size):
     radonVal = []
     radAngle = np.deg2rad(angle)
     maxSize = int(math.sqrt(size**2 + size**2))
-    assert(radAngle >= 0 and radAngle <= np.pi), "invalid angle"
+    #If angle is below 0 or higher or equal to 180, sends an error
+    assert(radAngle >= 0 and radAngle < np.pi), "invalid angle"
+    #If angle is 0
     if radAngle == 0:
         for x in range(0, size):
             pVal = 0
             for y in range(0,size):
                 pVal += img[y][x]
             radonVal.append(pVal)
+    #if angle is 90
     elif radAngle == np.pi*0.5:
         for y in range(0, size):
             pVal = 0
             for x in range(0,size):
                 pVal += img[y][x]
             radonVal.append(pVal)
+    #angle is above 0 and below 90
     elif radAngle > 0 and radAngle < np.pi*0.5:
         p = 0
         while p < maxSize:
@@ -32,10 +36,11 @@ def getRadon(img,angle,size):
                     pVal += img[y][x]
             radonVal.append(pVal)
             p += 1
+    #angle is above 90 and below 180
     elif radAngle > np.pi*0.5 and radAngle < np.pi:
-        halfMaxSize = int(maxSize/2)
-        p = -halfMaxSize
-        while p < halfMaxSize:
+        p = int(size*np.cos(radAngle))
+        pMax = int(size*np.sin(radAngle))
+        while p < pMax:
             pVal = 0
             for y in range(0,size):
                 x = int( (p - (y*np.sin(radAngle)))/(np.cos(radAngle)))
@@ -46,24 +51,28 @@ def getRadon(img,angle,size):
 
     return radonVal
 
+def displayRadon(img,segmentAngle,size):
+    sumAngle = 0
+    while sumAngle < 180:
+        radonTrans = getRadonAtAngle(img,sumAngle,size)
+        #print(radonTrans)
+        print(sumAngle)
+        xaxis = []
 
+        for xaxisVal in range(0,len(radonTrans)):
+            xaxis.append(xaxisVal)
+        plt.plot(xaxis,radonTrans,'go-',linewidth=1,markersize=1)
+        plt.show()
 
+        sumAngle += segmentAngle
         
 if __name__ == '__main__':
-    img = cv2.imread('imgforradon/square.png',0)
+    img = cv2.imread('imgforradon/triangle.png',0)
 
     #invering image to focus on black (for demo only)
     imagem = cv2.bitwise_not(img)
     height,width = imagem.shape
-    print(height)
-    radonTrans = getRadon(imagem,115,height)
-    print(radonTrans)
-    xaxis = []
-    for xaxisVal in range(0,len(radonTrans)):
-        xaxis.append(xaxisVal)
-    plt.plot(xaxis,radonTrans,'go-',linewidth=1,markersize=1)
-    plt.show()
-
+    displayRadon(imagem,22.5,height)
     cv2.imshow('image',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
