045a8bf565e9b5cdebc5a5b366c6b8687b6033f9
Hannestly <dooley994@gmail.com>
2018-06-07 19:09:07 -0400

    now functioning with irma samples, console displays file name and irma code

--- radon.py
+++ radon.py
@@ -2,6 +2,28 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import math
+import os
+import csv
+
+
+
+def getIrmaCode(filename):
+    name = filename.replace('.png','')
+    irmaCode = ''
+    with open('irmaSample/irmacode.csv','r') as f:
+        reader = csv.reader(f,dialect='excel',delimiter=';')
+        for row in reader:
+            for column in row:
+                if column == name:
+                    print(column,row[1])
+                    continue
+    with open('irmaSample/irmacode2.csv','r') as f:
+        reader = csv.reader(f,dialect='excel',delimiter=',')
+        for row in reader:
+            for column in row:
+                if column == name:
+                    print(column, row[5])
+                    continue
 
 
 
@@ -67,12 +89,28 @@ def displayRadon(img,segmentAngle,size):
         sumAngle += segmentAngle
         
 if __name__ == '__main__':
-    img = cv2.imread('imgforradon/triangle.png',0)
 
-    #invering image to focus on black (for demo only)
-    imagem = cv2.bitwise_not(img)
-    height,width = imagem.shape
-    displayRadon(imagem,22.5,height)
-    cv2.imshow('image',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    directory = 'irmaSample'
+    filepath = 'irmaSample/'
+    for filename in os.listdir(directory):
+        if filename.endswith('.png'):
+            absPath = filepath + filename
+            #print(absPath)
+            img = cv2.imread(absPath,0)
+            resizeImg = cv2.resize(img,(512,512))
+            resizeHeight,resizeWidth = resizeImg.shape
+            
+            #print(resizeHeight,resizeWidth)
+            getIrmaCode(filename)
+            cv2.imshow('image',img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            displayRadon(resizeImg,22.5,resizeHeight)
+
+            
+        else:
+            continue
+
+
+
+
